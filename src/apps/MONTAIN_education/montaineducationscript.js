0a8b47d746d7409465eec2e6d2c421d450b15766
luntras daniel <99122608+DANIELluntras@users.noreply.github.com>
2022-09-03 23:21:58 +0300

    chore: activate button for javascript

---
+++ src/apps/MONTAIN_education/montaineducationscript.js
@@ -0,0 +1,10 @@
+console.log("DLO");
+
+function extension() {
+    const table = document.getElementsByClassName("hidden-tabels");
+    if (table.style.display === "block") {
+        table.style.display = "none";
+    } else {
+        table.style.display = "block";
+    }
+}
--- src/script.js
+++ src/script.js
@@ -15,3 +15,34 @@ function myFunction() {
         console.log("merg");
     }
 }
+
+function myfunction() {
+    const boton = document.getElementById("operations__content--1");
+    if (boton.style.display === "block") {
+        boton.style.display = "none"
+
+
+    } else {
+        boton.style.display = "block"
+    }
+}
+
+
+// function close() {
+//     const botonclose = document.getElementById("operations__content--1");
+//     if (botonclose.style.display === "none") {
+//         botonclose.style.display === "block"
+//         console.log("click")
+//     }
+
+// }
+
+function bottton() {
+    const buttonsky2 = document.getElementById("operations__content--2");
+    if (buttonsky2.style.display === "block") {
+        buttonsky2.style.display = "none"
+
+    } else {
+        buttonsky2.style.display = "block"
+    }
+}
