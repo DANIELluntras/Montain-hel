a1df72112ce0dd6692555a0b9f109c639cc40003
luntras daniel <99122608+DANIELluntras@users.noreply.github.com>
2022-09-04 08:41:27 +0300

    chore:delet 5 line

--- src/apps/MONTAIN_education/montaineducationscript.js
+++ src/apps/MONTAIN_education/montaineducationscript.js
@@ -2,9 +2,13 @@ console.log("DLO");
 
 function extension() {
     const table = document.getElementsByClassName("hidden-tabels");
+
     if (table.style.display === "block") {
         table.style.display = "none";
+
     } else {
         table.style.display = "block";
     }
 }
+
+// element.classList.remove("mystyle");
